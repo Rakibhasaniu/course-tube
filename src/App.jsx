e588ceadb5c1e39ca0f28d535074fe52bb97d5a1
Rakibhsaniu <rakibhsanoyoniu@gmail.com>
2023-09-30 16:33:48 +0600

    usePlaylistHooks successfully finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { useEffect } from "react";
 import getPlaylist from "./api/Api";
 
 const App = () => {
-    useEffect(() => {
-        getPlaylist('PL_XxuZqN0xVBPhR5bjBIKyBjTo8pK99gN')
-            .then(res => console.log(res))
-    }, [])
+
     return (
         <div>
             <h1>Hello WOrld</h1>
---
+++ src/hooks/usePlaylist.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import getPlaylist from "../api/Api";
+
+const usePlaylist = () => {
+    const [state, setState] = useState({
+        playLists: {},
+        recentPlaylists: [],
+        favorites: []
+    })
+    const getPlaylistById = async (playlistId) => {
+        const result = await getPlaylist(playlistId)
+        result = result.map(item => {
+            const { channelId, title, description, thumbnails: { medium }, channelTitle, } = item.snippet;
+            return {
+                channelId, title, description, thumbnail: medium,
+                channelTitle, contentDetails: item.contentDetails
+            }
+        })
+        setState(prev => ({
+            ...prev,
+            playLists: {
+                ...prev.playLists,
+                [playlistId]: result
+            }
+        }))
+    }
+    const addToFavorite = (playlistId) => {
+        setState(prev => ({
+            ...prev,
+            favorites: [
+                ...prev,
+                playlistId
+            ]
+        }))
+    }
+    const addToRecent = (playlistId) => {
+        setState(prev => ({
+            ...prev,
+            recentPlaylists: [
+                ...prev,
+                playlistId
+            ]
+        }))
+    }
+    const getPlaylistsByIds = (ids = []) => {
+        return ids.map(id => state.playLists[id])
+    }
+    return {
+        playLists: state.playLists,
+        favorites: getPlaylistsByIds(state.favorites),
+        recentPlaylists: getPlaylistsByIds(state.recentPlaylists),
+        getPlaylistById,
+        addToFavorite,
+        addToRecent
+
+    }
+};
+
+export default usePlaylist;
